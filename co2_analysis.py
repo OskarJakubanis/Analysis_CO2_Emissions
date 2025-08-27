4bfaac2c54fe3c8a2ce7a328c5ac34dd36013e3b
Oskar <oskar.jakubanis@gmail.com>
2025-08-27 18:51:14 +0200

    Update co2_analysis.py

--- co2_analysis.py
+++ co2_analysis.py
@@ -14,10 +14,11 @@ conn = sqlite3.connect(r"C:\Users\Dell\Downloads\population\ANALYSIS_CO2.db")
 
 # Create dataset "growth" to add four columns: co2_2000, co2_2023, co2_growth_pct, co2_growth_nominal
 # Ensure country, co2, and year contain valid values (not NULL) to avoid missing data
-query_emissions = """
+query_growth_pct = """
 WITH growth AS (
     SELECT
         country,
+        population,
         MAX(CASE WHEN year = 2000 THEN CAST(co2 AS REAL) END) AS co2_2000,
         MAX(CASE WHEN year = 2023 THEN CAST(co2 AS REAL) END) AS co2_2023
     FROM emissions
@@ -28,10 +29,11 @@ WITH growth AS (
 )
 SELECT
     country,
+    population,
     co2_2000,
     co2_2023,
-    ROUND((co2_2023 / co2_2000 - 1) * 100, 2) AS co2_growth_pct
-    ROUND((co2_2023 - co2_2000) As co2_growth_nominal
+    ROUND((co2_2023 / co2_2000 - 1) * 100, 2) AS co2_growth_pct,
+    ROUND((co2_2023 - co2_2000), 2) As co2_growth_nominal
 FROM growth
 ORDER BY co2_growth_pct DESC
 LIMIT 100;
@@ -39,26 +41,32 @@ LIMIT 100;
 
 # Top 100 percent growth in CO2 emissions (2000–2023) 
 # Create a DataFrame from the SQL query results and export it to a CSV file
-df_growth = pd.read_sql_query(query_growth, conn)
-df_growth.to_csv("top100_growth.csv", index=False, header=True)
+df_growth_pct = pd.read_sql_query(query_growth_pct, conn)
+df_growth_pct.to_csv("top100_growth.csv", index=False, header=True)
 
 # Top 100 percent decline in CO2 emissions (2000–2023) 
 # Create a DataFrame from the SQL query results and export it to a CSV file
-query_decline_pct = query_growth.replace("DESC", "ASC")
+query_decline_pct = query_growth_pct.replace("DESC", "ASC")
 df_decline_pct = pd.read_sql_query(query_decline_pct, conn)
 df_decline_pct.to_csv("top100_decline.csv", index=False, header=True)
 
+# Top 100 nominal growth in CO2 emissions (2000–2023) 
+# Create a DataFrame from the SQL query results and export it to a CSV file
+query_nominal_growth = query_growth_pct.replace("ORDER BY co2_growth_pct DESC", "ORDER BY co2_growth_nominal DESC")
+df_nominal_growth = pd.read_sql_query(query_nominal_growth, conn)
+df_nominal_growth.to_csv("top100_growth_nominal.csv", index=False, header=True)
+
 # Top 100 nominal decline in CO2 emissions (2000–2023) 
 # Create a DataFrame from the SQL query results and export it to a CSV file
-query_nominal_decline = query_growth.replace("ORDER BY co2_growth_pct DESC", "ORDER BY co2_growth_nominal ASC")
+query_nominal_decline = query_growth_pct.replace("ORDER BY co2_growth_pct DESC", "ORDER BY co2_growth_nominal ASC")
 df_nominal_decline = pd.read_sql_query(query_nominal_decline, conn)
 df_nominal_decline.to_csv("top100_decline_nominal.csv", index=False, header=True)
 
 # Top 100 most populous countries in 2023 and CO2 changes (2000–2023) 
 # Create a DataFrame from the SQL query results and export it to a CSV file
-query_pop = query_growth.replace("ORDER BY co2_growth_pct DESC", "ORDER BY population DESC")
-df_pop = pd.read_sql_query(query_pop, conn)
-df_pop.to_csv("top10_population_co2.csv", index=False, header=True)
+query_population = query_growth_pct.replace("ORDER BY co2_growth_pct DESC", "ORDER BY population DESC")
+df_pop = pd.read_sql_query(query_population, conn)
+df_pop.to_csv("top100_population_co2.csv", index=False, header=True)
 
 # Close the database connection
 conn.close()
